5506bfa06c9e8f3e0c94b1c48be27386ed7b0789
Alanis <reisalana76@gmail.com>
2024-12-03 11:34:31 -0300

    Add files via upload

---
+++ Aula 02/aula02.py
@@ -0,0 +1,10 @@
+print("seja bem vindo")
+nome_usuario = "alana reis" # tipo => str = string
+idade_usuario = 20 # tipo -> int = integer
+altura_usuario = 1.73 # tipo -> float = ponto flutante
+maior_idade = True # tipo -> bool = boolean
+
+print('seja bem vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('você tem', altura_usuario, 'metros')
+print('voce é maior de idade?', maior_idade)
